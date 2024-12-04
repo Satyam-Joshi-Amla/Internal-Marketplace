6cc0b380428a0146d761d79e618f84d15de640dc
Vipul Chandrakapure <vipul.chandrakapure@amla.io>
2024-12-04 19:34:43 +0530

    03-12 work

--- AmlaMarketPlace/Controllers/AccountController.cs
+++ AmlaMarketPlace/Controllers/AccountController.cs
@@ -82,7 +82,7 @@ namespace AmlaMarketPlace.Controllers
                             // Sign in synchronously
                             HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties
                             {
-                                IsPersistent = true,
+                                IsPersistent = signInViewModel.RememberMe,
                                 ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
                             }).GetAwaiter().GetResult();
 
