10a173a92b213fd8b2c2ffa291a96b1e0e0454d6
Vipul Chandrakapure <vipul.chandrakapure@amla.io>
2024-12-05 11:25:13 +0530

    Profile

---
+++ AmlaMarketPlace.Models/ViewModels/Profile/ProfileDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmlaMarketPlace.Models.ViewModels.Profile
+{
+    public class ProfileDetailsViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
