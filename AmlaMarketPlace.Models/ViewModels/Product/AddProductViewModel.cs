1b6b3ef913da2f3346cd98f14c3f78a679d8b8d1
Satyam Joshi <satyam.joshi@amla.io>
2024-12-05 16:26:49 +0530

    Image validation ith other add product validations

--- AmlaMarketPlace.Models/ViewModels/Product/AddProductViewModel.cs
+++ AmlaMarketPlace.Models/ViewModels/Product/AddProductViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AmlaMarketPlace.Models.ViewModels.Validations;
 
 using Microsoft.AspNetCore.Http;
 
@@ -14,18 +15,22 @@ namespace AmlaMarketPlace.Models.ViewModels.Product
         public int UserId { get; set; }
 
         [Required]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 500 characters.")]
         public string Name { get; set; }
 
         [Required]
+        [Range(0.01, 9999999, ErrorMessage = "Price must between 0.01 and 99,99,999.")]
         public decimal Price { get; set; }
 
-        [Required]
+        [StringLength(50000, ErrorMessage = "Description can't be longer than 50000 characters.")]
         public string Description { get; set; }
 
         [Required]
+        [Range(1, 99999, ErrorMessage = "Inventory must be between 1 and 99999.")]
         public int Inventory { get; set; }
 
         [Required]
+        [ImageValidation(ErrorMessage = "Only image files (JPG, PNG) are allowed.")]
         public IFormFile Image { get; set; }
     }
 }
---
+++ AmlaMarketPlace.Models/ViewModels/Validations/ImageValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace AmlaMarketPlace.Models.ViewModels.Validations
+{
+    public class ImageValidation : ValidationAttribute
+    {
+        private readonly string[] _allowedExtensions = { ".jpg", ".jpeg", ".png" };
+
+        public ImageValidation() : base("The file must be an image (JPG, PNG).")
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            // Get the file extension
+            var fileExtension = Path.GetExtension(file.FileName).ToLower();
+
+            // Check if the extension is valid
+            if (Array.Exists(_allowedExtensions, ext => ext == fileExtension))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(ErrorMessage ?? "Invalid file type. Only image files (JPG, PNG) are allowed.");
+            }
+        }
+    }
+}
