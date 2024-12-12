1a545c4d0ba540bc5a91a1cba6f15b141431409b
Vipul Chandrakapure <vipul.chandrakapure@amla.io>
2024-12-12 11:25:15 +0530

    Admin Panel changes

---
+++ AmlaMarketPlace.DAL/Data/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmlaMarketPlace.DAL.Data;
+
+public partial class Comment
+{
+    public int CommentId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public string Comment1 { get; set; } = null!;
+
+    public DateTime Date { get; set; }
+}
